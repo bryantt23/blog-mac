11fbb2596aef5635118506156b1c70aa904c2e13
bryant tee <bryantt23@gmail.com>
2015-02-23 01:42:10 -0800

    Revert "Add pages"
    This reverts commit da0fc4f977fab81ad2b19c72f14e2296a1262876.
    
    Conflicts:
    	config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -4,9 +4,15 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments
   end
+<<<<<<< HEAD
 
   root "posts#index"
 
   get '/about', to: 'pages#about'
+=======
+  
+  root 'posts#index'
+  
+>>>>>>> parent of da0fc4f... Add pages
 
 end
