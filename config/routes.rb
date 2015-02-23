993da6a93c5ed83dd64a4f465668f30308124fbc
bryant tee <bryantt23@gmail.com>
2015-02-23 02:26:10 -0800

    Add comments

--- config/routes.rb
+++ config/routes.rb
@@ -1,34 +1,9 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
 
-  devise_for :users
   resources :posts do
     resources :comments
   end
-<<<<<<< HEAD
 
   root "posts#index"
-
-  get '/about', to: 'pages#about'
-=======
-=======
   
-  
-<<<<<<< HEAD
-<<<<<<< HEAD
-  
-  resources :posts
->>>>>>> parent of b18f1f0... Add comments
-  
-  root 'posts#index'
-  
-<<<<<<< HEAD
->>>>>>> parent of da0fc4f... Add pages
-=======
->>>>>>> parent of da0fc4f... Add pages
-=======
->>>>>>> parent of da0fc4f... Add pages
-=======
->>>>>>> parent of da0fc4f... Add pages
-
 end
