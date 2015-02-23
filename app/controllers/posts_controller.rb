3715ffb45dc2874028cbaf076691dc937b929671
bryant tee <bryantt23@gmail.com>
2015-02-22 23:55:27 -0800

    Added posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,11 +1,28 @@
 class PostsController < ApplicationController
   
+  def show
+    @post = Post.find(params[:id])
+  end
+  
   def index
     # @post = Post.all
+    @posts = Post.all.order('created_at DESC')
   end
   
   def new
     # @post = Post.new
   end
   
+  def create
+    @post = Post.new(post_params)
+ 
+    @post.save
+    redirect_to @post
+end
+  
+  private
+ 
+  def post_params
+    params.require(:post).permit(:title, :body)
+  end
 end
